e73a02cd59e0b5a5ee0c96a14ba39d73742a9524
Matthew Kongsiri <m_kongsiri@hotmail.com>
2017-07-01 13:06:57 -0400

    Added detail view controller

--- PokemonDetailVC.swift
+++ PokemonDetailVC.swift
@@ -9,11 +9,15 @@
 import UIKit
 
 class PokemonDetailVC: UIViewController {
+    
+    @IBOutlet weak var nameLbl: UILabel!
+    
+    var pokemon: Pokemon!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
         
+        nameLbl.text = pokemon.name
     }
 
 }
