d33904b7086dd187a633f76ed131e0a297d41538
Matthew Kongsiri <m_kongsiri@hotmail.com>
2017-06-30 22:42:30 -0400

    UICollectionView and more assets

---
+++ Pokedex/Pokemon.swift
@@ -0,0 +1,29 @@
+//
+//  Pokemon.swift
+//  Pokedex
+//
+//  Created by Matthew Kongsiri on 6/30/17.
+//  Copyright © 2017 Matthew Kongsiri. All rights reserved.
+//
+
+import Foundation
+
+class Pokemon {
+    
+    private var _name: String!
+    private var _pokedexId: Int!
+    
+    var name: String {
+        return _name
+    }
+    
+    var pokedexId: Int {
+        return _pokedexId
+    }
+    
+    init(name: String, pokedexId: Int) {
+        self._name = name
+        self._pokedexId = pokedexId
+    }
+    
+}
