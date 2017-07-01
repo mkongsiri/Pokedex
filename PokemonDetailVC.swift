525f0fb74e5f0b559ce1e1ea7f92ad49874d97af
Matthew Kongsiri <m_kongsiri@hotmail.com>
2017-07-01 12:56:43 -0400

    Added search filtering

---
+++ PokemonDetailVC.swift
@@ -0,0 +1,19 @@
+//
+//  PokemonDetailVC.swift
+//  Pokedex
+//
+//  Created by Matthew Kongsiri on 7/1/17.
+//  Copyright © 2017 Matthew Kongsiri. All rights reserved.
+//
+
+import UIKit
+
+class PokemonDetailVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+
+}
